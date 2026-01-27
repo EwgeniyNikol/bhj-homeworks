77d7ac337c6136db9f03241f5dfafc864702ac4c
Ewgeny Ananyev <ewgena81@gmail.com>
2026-01-27 16:05:12 +0300

    Загрузка заданий Асинхронные запросы-исправления 1-е задание

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,46 +1,75 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const loader = document.getElementById('loader');
-  const itemsContainer = document.getElementById('items');
+const init = () => {
+  const loaderEl = document.getElementById('loader');
+  const itemsEl = document.getElementById('items');
+
+  loadAndRender({
+    url: 'https://students.netoservices.ru/nestjs-backend/slow-get-courses',
+    loaderEl,
+    itemsEl,
+  });
+};
+
+const loadAndRender = ({ url, loaderEl, itemsEl }) => {
+  loaderEl.classList.add('loader_active');
   
-  function displayCurrencies(valutes) {
-    itemsContainer.innerHTML = '';
-    
-    for (const key in valutes) {
-      const item = document.createElement('div');
-      item.className = 'item';
-      
-      item.innerHTML = `
-        <div class="item__code">${valutes[key].CharCode}</div>
-        <div class="item__value">${valutes[key].Value.toFixed(4)}</div>
-        <div class="item__currency">руб.</div>
-      `;
-      
-      itemsContainer.appendChild(item);
-    }
-  }
-  
-  loader.classList.add('loader_active');
-  
-  const cachedData = localStorage.getItem('currencyData');
+  const cachedData = getCachedData();
   if (cachedData) {
-    const data = JSON.parse(cachedData);
-    displayCurrencies(data.response.Valute);
-    loader.classList.remove('loader_active');
+    renderCurrenciesMarkup(itemsEl, cachedData);
   }
   
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+  fetchCurrencies(url).then(data => {
+    const currencies = extractCurrenciesData(data);
+    cacheData(data);
+    renderCurrenciesMarkup(itemsEl, currencies);
+    loaderEl.classList.remove('loader_active');
+  });
+};
+
+const getCachedData = () => {
+  const cache = localStorage.getItem('currencyData');
+  if (!cache) return null;
   
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      
-      localStorage.setItem('currencyData', JSON.stringify(data));
-      displayCurrencies(data.response.Valute);
-    }
-    
-    loader.classList.remove('loader_active');
-  };
+  try {
+    const data = JSON.parse(cache);
+    return extractCurrenciesData(data);
+  } catch {
+    return null;
+  }
+};
+
+const cacheData = (data) => {
+  localStorage.setItem('currencyData', JSON.stringify(data));
+};
+
+const fetchCurrencies = (url) => {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      resolve(JSON.parse(xhr.responseText));
+    };
+    xhr.send();
+  });
+};
+
+const extractCurrenciesData = (data) => {
+  return data.response.Valute;
+};
+
+const renderCurrenciesMarkup = (container, currencies) => {
+  container.innerHTML = '';
   
-  xhr.send();
-});
+  for (const key in currencies) {
+    const currency = currencies[key];
+    const item = document.createElement('div');
+    item.className = 'item';
+    item.innerHTML = `
+      <div class="item__code">${currency.CharCode}</div>
+      <div class="item__value">${currency.Value.toFixed(4)}</div>
+      <div class="item__currency">руб.</div>
+    `;
+    container.appendChild(item);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', init);
